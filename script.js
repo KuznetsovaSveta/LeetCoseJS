adcb28ec8e60221ec4a9b4d6b93b78869977b066
Kuznetsova Svetlana <kuznetsovasveta0408@gmail.com>
2022-04-27 12:05:06 +0300

    sqrt(x)

--- script.js
+++ script.js
@@ -111,3 +111,17 @@ let romanToInt = function(s) {
 
 romanToInt("MCMXCIV");
 
+
+
+//5
+//Given a non-negative integer x, compute and return the square root of x.
+// Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+// Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+let mySqrt = function(x) {
+    for (let i = 0; i <= x; i++) {
+        if (i * i === x) return i;
+        if ((i * i < x) && ((i + 1) * (i + 1) > x)) return i;
+    }
+};
+
+mySqrt(9);
